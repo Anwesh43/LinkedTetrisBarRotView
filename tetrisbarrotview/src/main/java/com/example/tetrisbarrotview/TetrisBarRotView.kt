9ebab76f790add87dc5df4fbc03b8efaf1a829f8
Anwesh43 <anweshthecool0@gmail.com>
2020-12-31 03:02:29 +0530

    adding TetrisBarRotView class

--- tetrisbarrotview/src/main/java/com/example/tetrisbarrotview/TetrisBarRotView.kt
+++ tetrisbarrotview/src/main/java/com/example/tetrisbarrotview/TetrisBarRotView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawTBRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTetrisBarRot(scale, w, h, paint)
 }
+
+class TetrisBarRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
