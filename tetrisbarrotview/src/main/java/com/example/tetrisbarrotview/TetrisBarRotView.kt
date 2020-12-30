5022d8928f74d2cdd59bce20ab2a88b30d1d53f0
Anwesh43 <anweshthecool0@gmail.com>
2020-12-31 03:00:55 +0530

    adding Canvas extension methods

--- tetrisbarrotview/src/main/java/com/example/tetrisbarrotview/TetrisBarRotView.kt
+++ tetrisbarrotview/src/main/java/com/example/tetrisbarrotview/TetrisBarRotView.kt
@@ -30,3 +30,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTetrisBarRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val barH : Float = Math.min(w, h) / barHFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(0f, (h / 2 - barH) - (h / 2 - 2 * barH) * sf2)
+        drawRect(RectF(w / 2 * (1 - sf1), 0f, w / 2, barH), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTetrisBarRot(scale, w, h, paint)
+}
